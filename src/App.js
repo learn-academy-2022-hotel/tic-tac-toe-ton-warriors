6e78b7d49631554072fbb63af0e85e86c7065f2e
oagomezz <ogomezbarragan437@gmail.com>
2023-01-03 14:55:34 -0800

    Attempting to rewrite code to use ternary functions

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import Square from './components/Square'
 import './App.css'
 
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [square, setSquare] = useState(Array(9).fill(null))
+    const [isX, setIsX] = useState(true);
+    const handleClick = () => {
+      if
+    }
 
-  const handleGamePlay= (clickedSquare) => {
-    let updateBoard =[...board]
-    if(updateBoard[clickedSquare] === "❌"){
-      updateBoard[clickedSquare]= "⭕️"
-      setBoard(updateBoard)
-    }else{
-    updateBoard[clickedSquare]= "❌"
-    setBoard(updateBoard)
+  // const handleGamePlay= (clickedSquare) => {
+  //   let updateBoard =[...board]
+  //   if(updateBoard[clickedSquare] === "❌"){
+  //     updateBoard[clickedSquare]= "⭕️"
+  //     setBoard(updateBoard)
+  //   }else{
+  //   updateBoard[clickedSquare]= "❌"
+  //   setBoard(updateBoard)
+  //   }
+  // }
+    const renderSquare = (i) => {
+      return <Square value={squares[i] onClick={()=> handleClick(i)} />
     }
   }
-  
-
   return (
     <>
       <h1>Tic Tac Toe</h1>
